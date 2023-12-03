ccf195902893d703869e06deb7a9c3216b21160c
Lam123-ok <vuxuanlam0113@gmail.com>
2023-12-03 18:21:35 +0700

    Code_BE_4_deploy_2

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const OrderController = require('../controllers/OrderController');
-const { authUserMiddleware, authMiddleware } = require("../middleware/authMiddleWare");
+const { authUserMiddleware, authMiddleware } = require("../middleware/authMiddleware");
 
 router.post('/create',authUserMiddleware, OrderController.createOrder)
 router.get('/get-all-order/:id',authUserMiddleware, OrderController.getAllOrderDetails)
--- src/routes/PaymentRouter.js
+++ src/routes/PaymentRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router()
-const { authUserMiddleware } = require("../middleware/authMiddleWare");
 const dotenv = require("dotenv");
 
 dotenv.config()
