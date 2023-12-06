072614f76a29e1ef9fe63cae00d39d1228b8e1d9
Lam123-ok <vuxuanlam0113@gmail.com>
2023-12-06 21:13:27 +0700

    Code_BE_4_deploy

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -28,7 +28,8 @@ const authMiddleware = (req,res,next) =>{
 
 const authUserMiddleware = (req,res,next) =>{
   const token = req.headers.token.split(' ')[1] // Thêm dấu ? trước split 
-  const userId = req.params.id
+  const userId = req.body.userId
+  console.log(req);
   jwt.verify(token, process.env.ACCESS_TOKEN, function(err,user){ 
     if(err){
       return res.status(404).json({
